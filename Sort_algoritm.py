7297eeaa94ed8b4f3a5fe98a3e29eaf86d9a564e
Yaroslav-Syberia <56403906+Yaroslav-Syberia@users.noreply.github.com>
2019-11-13 19:31:27 +0300

    Add files via upload
    my realisation of sorting algoritms

---
+++ Sort_algoritm.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 15:31:59 2019
+
+@author: Y
+"""
+def bubble_sort(arg):
+    """original bubble sorting algoritm 
+    \n\r arg:lists,arrays 
+    \n\r returns sorted object"""
+    if type(arg) != list:
+        print("wrong type")
+    else:
+        i = 1
+        while(i<=len(arg)-1):
+            k = 0
+            while(k<=len(arg)-i-1):
+                if arg[k]>arg[k+1]:
+                    arg[k],arg[k+1] = arg[k+1],arg[k]
+                k = k+1
+            i =i + 1
+        return arg
+
+def bubble_sort_plus(arg):
+    """modificated bubble sorting algoritm
+    \n\r this realisation checks is objects sorted and can 
+    \n\r return answer earlier than original algoritm
+    \n\r arg:lists,arrays 
+    \n\r returns sorted object"""
+    if type(arg) != list:
+        print("wrong type")
+    else:
+        i = 1
+
+        while(i<=len(arg)-1):
+            k = 0
+            f=0
+            while(k<=len(arg)-i-1):
+                if arg[k]>arg[k+1]:
+                    arg[k],arg[k+1] = arg[k+1],arg[k]
+                    f=1
+                k = k+1
+            if f==0:
+                return arg
+            else:
+                i =i + 1
+        return arg
+        
+help(bubble_sort)                  
+a=[21,14,18,25,9]
+b=bubble_sort_plus(a)
+print("b=",b)
